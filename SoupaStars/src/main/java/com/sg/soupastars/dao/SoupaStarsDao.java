152e8bbfeadef445f152001e60d1acd8672552b4
William Henry Long <william.h.long.1@gmail.com>
2016-11-15 15:09:46 -0500

    a

--- SoupaStars/src/main/java/com/sg/soupastars/dao/SoupaStarsDao.java
+++ SoupaStars/src/main/java/com/sg/soupastars/dao/SoupaStarsDao.java
@@ -28,8 +28,8 @@ public interface SoupaStarsDao {
     public void removePost(int postId);
     
     
-   public List<Post> searchPosts(Map<SearchTerm,String> criteria);
+   //public List<Post> searchPosts(Map<SearchTerm,String> criteria);
    
-   public List<PostCount> getPostCounts();
+   //public List<PostCount> getPostCounts();
     
 }
--- SoupaStars/src/main/java/com/sg/soupastars/dao/SoupaStarsDaoDBImpl.java
+++ SoupaStars/src/main/java/com/sg/soupastars/dao/SoupaStarsDaoDBImpl.java
@@ -5,10 +5,54 @@
  */
 package com.sg.soupastars.dao;
 
+import com.sg.soupastars.model.Post;
+import java.util.List;
+import java.util.Map;
+
 /**
  *
  * @author apprentice
  */
-public class SoupaStarsDaoDBImpl {
+public class SoupaStarsDaoDBImpl implements SoupaStarsDao {
+
+    @Override
+    public Post addPost(Post post) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Post getPostById(int PostId) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Post> getAllPostsByTitle(String titleToSearch) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Post> getAllPosts() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void updatePost(Post post) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void removePost(int postId) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+//    @Override
+//    public List<Post> searchPosts(Map<SearchTerm, String> criteria) {
+//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+//    }
+//
+//    @Override
+//    public List<PostCount> getPostCounts() {
+//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+//    }
     
 }
