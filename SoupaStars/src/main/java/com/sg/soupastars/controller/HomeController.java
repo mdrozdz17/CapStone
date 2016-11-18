ab1c395880ecbbf2d4caa1885232254a440c7ef6
Matthew Drozdz <Matthew.Drozdz@LibertyMutual.com>
2016-11-18 11:27:42 -0500

    Added image to error.jsp page, Added comment to the home controller and worked on user controller

--- SoupaStars/src/main/java/com/sg/soupastars/controller/HomeController.java
+++ SoupaStars/src/main/java/com/sg/soupastars/controller/HomeController.java
@@ -5,6 +5,7 @@
  */
 package com.sg.soupastars.controller;
 
+import com.sg.soupastars.dao.SoupaStarsCommentDao;
 import com.sg.soupastars.model.Post;
 import java.io.FileNotFoundException;
 import java.util.List;
@@ -20,6 +21,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import com.sg.soupastars.dao.SoupaStarsPostDao;
+import com.sg.soupastars.model.Comment;
 
 /**
  *
@@ -28,11 +30,14 @@ import com.sg.soupastars.dao.SoupaStarsPostDao;
 @Controller
 public class HomeController {
     
-    private SoupaStarsPostDao dao;
+    private SoupaStarsPostDao pdao;
+     
+   private SoupaStarsCommentDao cdao;
+    
     
     @Inject
-    public HomeController(SoupaStarsPostDao dao){
-        this.dao = dao;
+    public HomeController(SoupaStarsPostDao pdao, SoupaStarsCommentDao cDao){
+        this.pdao = pdao;
     }
     
     @RequestMapping(value={"home","/"}, method=RequestMethod.GET)
@@ -42,12 +47,12 @@ public class HomeController {
     
     
     // - Retrieve a Post by Id (GET)
-//        - /dvd/{dvdId}
-//        - Response Body: DVD in JSON  
+//        - /post/{postId}
+//        - Response Body: POST in JSON  
     @RequestMapping(value = "/post/{id}", method = RequestMethod.GET)
     @ResponseBody
     public Post getPost(@PathVariable("id") int id) {
-        return dao.getPostById(id);
+        return pdao.getPostById(id);
     }
 
 //- Create a Post (POST)
@@ -57,7 +62,7 @@ public class HomeController {
     @ResponseStatus(HttpStatus.CREATED)
     @ResponseBody
     public Post createPost(@Valid @RequestBody Post post) {
-        dao.addPost(post);
+        pdao.addPost(post);
         return post;
     }
 
@@ -67,7 +72,7 @@ public class HomeController {
     @RequestMapping(value = "/post/{id}", method = RequestMethod.DELETE)
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deletePost(@PathVariable("id") int id) {
-        dao.removePost(id);
+        pdao.removePost(id);
     }
 
 //- Update a Post (PUT)
@@ -77,27 +82,77 @@ public class HomeController {
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void updatePost(@PathVariable("id") int id, @Valid @RequestBody Post post) {
         post.setPostId(id);
-        dao.updatePost(post);
+        pdao.updatePost(post);
     }
     
 //- Retrieve ALL Posts (GET)
 //        - /post
-//        -ResponseBody: Array of Contacts, in JSON form
     @RequestMapping(value="/post", method=RequestMethod.GET)
-    @ResponseBody public List<Post> getAllDVDS(){
-        return dao.getAllPosts();
+    @ResponseBody public List<Post> getAllPots(){
+        return pdao.getAllPosts();
     }
     
     
     @RequestMapping(value = "/displayPost", method = RequestMethod.GET)
     public String displayPost(Model model) throws FileNotFoundException {
-        // Get all DVD
-        List<Post> allPost = dao.getAllPosts();
-        // attach the DVD to Model for Spring MVC to pass along to the view
+        List<Post> allPost = pdao.getAllPosts();
         model.addAttribute("post", allPost);
 
         // return the logical view
         return "displayPost";
+    }
+    
+    // Comments 
+   
+    
+    @RequestMapping(value = "/comment/{id}", method = RequestMethod.GET)
+    @ResponseBody
+    public Comment getComment(@PathVariable("id") int id) {
+        return cdao.getCommentById(id);
+    }
+
+//- Create a Comment (POST)
+    @RequestMapping(value = "/comment", method = RequestMethod.POST)
+    @ResponseStatus(HttpStatus.CREATED)
+    @ResponseBody
+    public Comment createComment(@Valid @RequestBody Comment comment) {
+        cdao.addComment(comment);
+        return comment;
+    }
+
+//- Delete a Comment (DELETE)
+    @RequestMapping(value = "/comment/{id}", method = RequestMethod.DELETE)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteComment(@PathVariable("id") int id) {
+        cdao.removeComment(id);
+    }
+
+//- Update a Comment (PUT)
+    @RequestMapping(value = "/comment/{id}", method = RequestMethod.PUT)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void updateComment(@PathVariable("id") int id, @Valid @RequestBody Comment comment) {
+        comment.setCommentId(id);
+        cdao.updateComment(comment);
+    }
+    
+//- Retrieve ALL Comments (GET)
+    @RequestMapping(value="/comment", method=RequestMethod.GET)
+    @ResponseBody public List<Comment> getAllComments(){
+        return cdao.getAllComments();
+    }
+    
+   
+    
+    // I am unsure about this. The RequestMapping cannot be /displayPost since it
+    // would be ambigious. However we do not have a displayComment page.
+    
+    @RequestMapping(value = "/displayComment", method = RequestMethod.GET)
+    public String displayComment(Model model) throws FileNotFoundException {
+        List<Comment> allComment = cdao.getAllComments();
+        model.addAttribute("comment", allComment);
+
+        // return the logical view
+        return "displayComment";
     }
   
 }
--- SoupaStars/src/main/java/com/sg/soupastars/controller/UserController.java
+++ SoupaStars/src/main/java/com/sg/soupastars/controller/UserController.java
@@ -5,10 +5,81 @@
  */
 package com.sg.soupastars.controller;
 
+import com.sg.soupastars.dao.SoupaStarsUserDao;
+import com.sg.soupastars.model.User;
+import java.io.FileNotFoundException;
+import java.util.List;
+import javax.inject.Inject;
+import javax.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 /**
  *
  * @author apprentice
  */
+@Controller
 public class UserController {
-    
+
+    private SoupaStarsUserDao dao;
+
+    @Inject
+    public UserController(SoupaStarsUserDao dao) {
+        this.dao = dao;
+    }
+
+    // - Retrieve a User by Id (GET)
+    @RequestMapping(value = "/user/{id}", method = RequestMethod.GET)
+    @ResponseBody
+    public User getUserName(@PathVariable("id") int id) {
+        return dao.getUserNameById(id);
+    }
+
+    //- Create a User (POST)
+    @RequestMapping(value = "/user", method = RequestMethod.POST)
+    @ResponseStatus(HttpStatus.CREATED)
+    @ResponseBody
+    public User createUserName(@Valid @RequestBody User user) {
+        dao.addUserName(user);
+        return user;
+    }
+
+    //- Delete a User (DELETE)
+    @RequestMapping(value = "/user/{id}", method = RequestMethod.DELETE)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteUserName(@PathVariable("id") int id) {
+        dao.removeUserName(id);
+    }
+
+    //- Update a User (PUT)
+    @RequestMapping(value = "/user/{id}", method = RequestMethod.PUT)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void updateUserName(@PathVariable("id") int id, @Valid @RequestBody User user) {
+        user.setUserId(id);
+        dao.updateUserInfo(user);
+    }
+
+    //- Retrieve ALL Users (GET)
+    @RequestMapping(value = "/user", method = RequestMethod.GET)
+    @ResponseBody
+    public List<User> getAllUserNames() {
+        return dao.getAllUserNames();
+    }
+
+    @RequestMapping(value = "/displayUsers", method = RequestMethod.GET)
+    public String displayUserNames(Model model) throws FileNotFoundException {
+        List<User> allUsers = dao.getAllUserNames();
+        model.addAttribute("user", allUsers);
+
+        // return the logical view
+        return "displayUsers";
+    }
+
 }
