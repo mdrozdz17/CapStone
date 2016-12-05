7f8e06d61f53d30f77f5ee7426ca8592c12bad05
William Henry Long <william.h.long.1@gmail.com>
2016-12-05 10:15:25 -0500

    added tags to addPost

--- SoupaStars/src/main/java/com/sg/soupastars/dao/SoupaStarsPostDaoDBImpl.java
+++ SoupaStars/src/main/java/com/sg/soupastars/dao/SoupaStarsPostDaoDBImpl.java
@@ -10,6 +10,7 @@ import com.sg.soupastars.model.Post;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.BatchPreparedStatementSetter;
@@ -22,13 +23,12 @@ import org.springframework.transaction.annotation.Transactional;
  *
  * @author apprentice
  */
+public class SoupaStarsPostDaoDBImpl implements SoupaStarsPostDao {
 
-public class SoupaStarsPostDaoDBImpl implements SoupaStarsPostDao{
-   
     private static final String SQL_INSERT_POST = "insert into Post (Title, PostYear, PostMonth, PostDay, Author, PostBody, Category) values (?,?,?,?,?,?,?)";
     private static final String SQL_DELETE_POST = "delete from Post where PostID = ?";
     private static final String SQL_INSERT_TAG = "insert into Tag (TagBody) values (?)";
-    private static final String SQL_SELECT_POSTTAG = "insert into PostTag (PostID, TagID) values (?,?)";
+    private static final String SQL_INSERT_POSTTAG = "insert into PostTag (PostID, TagID) values (?,?)";
     private static final String SQL_SELECT_POST = "select * from Post where PostID =  ?";
     private static final String SQL_SELECT_TAG = "select * from Post where TagID =  ?";
     private static final String SQL_UPDATE_POST = "update Post set Title = ?, PostYear = ?, PostMonth = ?, PostDay = ?, Author = ? , PostBody = ?, Category = ? where PostID = ?";
@@ -40,8 +40,8 @@ public class SoupaStarsPostDaoDBImpl implements SoupaStarsPostDao{
     private static final String SQL_SELECT_POST_BY_SEARCHTERM = "select * from Post where Title like ? or Author like ? or PostBody like ? or Category like ?";
     private static final String SQL_INSERT_POSTS_TAGS = "select TagBody from PostTag join Tag using (TagID) where PostID = ?";
 
-    private static final String SQL_SELECT_TAGS_BY_POST = "select p.PostID, p.Title, p.PostYear, p.PostMonth, p.PostDay, p.Author, p.PostBody, p.Category \n" +
-"from Post p join PostTag on p.PostID = PostTag.PostID where PostTag.TagID = ?"; 
+    private static final String SQL_SELECT_TAGS_BY_POST = "select p.PostID, p.Title, p.PostYear, p.PostMonth, p.PostDay, p.Author, p.PostBody, p.Category \n"
+            + "from Post p join PostTag on p.PostID = PostTag.PostID where PostTag.TagID = ?";
     // #2a - Declare JdbcTemplate reference - the instance will be handed to us by Spring
     private JdbcTemplate jdbcTemplate;
     // #2b - We are using Setter Injection to direct Spring to hand us an instance of
@@ -52,71 +52,64 @@ public class SoupaStarsPostDaoDBImpl implements SoupaStarsPostDao{
         this.jdbcTemplate = jdbcTemplate;
     }
 
-
     @Override
     @Transactional(propagation = Propagation.REQUIRED)
-    public Post addPost(Post post) {  
-        jdbcTemplate.update(SQL_INSERT_POST, 
-        post.getTitle(),
-        post.getYear(),
-        post.getMonth(),
-        post.getDay(),
-        post.getAuthor(),
-        post.getBody(),
-        post.getCategory());
-        insertPostTags(post);
-        post.setPostId(jdbcTemplate.queryForObject("select LAST_INSERT_ID()", Integer.class));
-         
-        return post;
+    public Post addPost(Post post) {
+        jdbcTemplate.update(SQL_INSERT_POST,
+                post.getTitle(),
+                post.getYear(),
+                post.getMonth(),
+                post.getDay(),
+                post.getAuthor(),
+                post.getBody(),
+                post.getCategory());
         
+        post.setPostId(jdbcTemplate.queryForObject("select LAST_INSERT_ID()", Integer.class));
+        insertPostTags(post);
+        return post;
+
     }
-    
-    
-        private void insertPostTags(Post post) {
-        final int postId = post.getPostId(); // Assume for talking that we have a Book (bookId = 1) 
-        final List<String> tagList = post.getTagList(); // with 2 authors (Author Ids: [1,2])
+   
+    private void insertPostTags(Post post) {
+        int postId = post.getPostId(); // Assume for talking that we have a Book (bookId = 1) 
+        List<String> tagList = post.getTagList(); // with 2 authors (Author Ids: [1,2])
         // use the batchUpdate so we only make one call to the database
-        jdbcTemplate.batchUpdate(SQL_INSERT_TAG, new BatchPreparedStatementSetter() {
-            @Override
-            public void setValues(PreparedStatement ps, int i) throws SQLException {
-                // For reference:  "insert into books_authors (book_id, author_id) values(?, ?)";
-             //   ps.setInt(1, postId); // Set parameter 1 = value of book Id - 1 indicates 1st question mark
-                ps.setString(1, tagList.get(i)); // Set parameter 2 = the author[i] where i is the iteration; 2 indicates 2nd question mark
-                // NOTE: This handles the iteration for us - we don't need to do it manually
-            }
-            @Override
-            public int getBatchSize() {
-                return tagList.size();
-            }
-        });
+        
+        for (String tag : tagList) {
+            jdbcTemplate.update(SQL_INSERT_TAG, tag);
+     
+            int tagId = jdbcTemplate.queryForObject("select LAST_INSERT_ID()", Integer.class);
+            
+            
+            jdbcTemplate.update(SQL_INSERT_POSTTAG,postId, tagId);
+        }
     }
- 
 
     @Override
     public Post getPostById(int PostId) {
-          try {
-        Post post = jdbcTemplate.queryForObject(SQL_SELECT_POST, new PostMapper(), PostId);
-        List<String> tagList = (List<String>) jdbcTemplate.query(SQL_SELECT_TAGS_BY_POSTID, new TagMapper(), PostId);
-        post.setTagList(tagList);
-        List<Comment> commentList = jdbcTemplate.query(SQL_SELECT_COMMENTS_BY_POSTID, new CommentMapper(), post.postId);
-        post.setCommentList(commentList);
-        return post;
+        try {
+            Post post = jdbcTemplate.queryForObject(SQL_SELECT_POST, new PostMapper(), PostId);
+            List<String> tagList = (List<String>) jdbcTemplate.query(SQL_SELECT_TAGS_BY_POSTID, new TagMapper(), PostId);
+            post.setTagList(tagList);
+            List<Comment> commentList = jdbcTemplate.query(SQL_SELECT_COMMENTS_BY_POSTID, new CommentMapper(), post.postId);
+            post.setCommentList(commentList);
+            return post;
         } catch (EmptyResultDataAccessException ex) {
             return null;
         }
     }
-    
-        @Override
+
+    @Override
     public List<Post> read(Integer postId) {
 
-        List<Post> b = jdbcTemplate.query(SQL_SELECT_POSTTAG, new PostMapper(), postId);
+        List<Post> b = jdbcTemplate.query(SQL_INSERT_POSTTAG, new PostMapper(), postId);
 
         return b;
     }
 
     @Override
     public List<Post> getAllPostsByTitle(String titleToSearch) {
-          return jdbcTemplate.query(SQL_SELECT_POSTS_BY_TITLE, new PostMapper());
+        return jdbcTemplate.query(SQL_SELECT_POSTS_BY_TITLE, new PostMapper());
 
     }
 
@@ -124,7 +117,7 @@ public class SoupaStarsPostDaoDBImpl implements SoupaStarsPostDao{
     public List<Post> getAllPosts() {
         List<Post> postList = jdbcTemplate.query(SQL_SELECT_ALL_POSTS, new PostMapper());
         for (Post post : postList) {
-            List<String> tagList =  jdbcTemplate.query(SQL_SELECT_TAGS_BY_POSTID, new TagMapper(), post.postId);
+            List<String> tagList = jdbcTemplate.query(SQL_SELECT_TAGS_BY_POSTID, new TagMapper(), post.postId);
             post.setTagList(tagList);
             List<Comment> commentList = jdbcTemplate.query(SQL_SELECT_COMMENTS_BY_POSTID, new CommentMapper(), post.postId);
             post.setCommentList(commentList);
@@ -134,7 +127,7 @@ public class SoupaStarsPostDaoDBImpl implements SoupaStarsPostDao{
 
     @Override
     public void updatePost(Post post) {
-               jdbcTemplate.update(SQL_UPDATE_POST,
+        jdbcTemplate.update(SQL_UPDATE_POST,
                 post.getTitle(),
                 post.getYear(),
                 post.getMonth(),
@@ -143,7 +136,7 @@ public class SoupaStarsPostDaoDBImpl implements SoupaStarsPostDao{
                 post.getBody(),
                 post.getCategory(),
                 post.getPostId());
-                
+
     }
 
     @Override
@@ -151,27 +144,24 @@ public class SoupaStarsPostDaoDBImpl implements SoupaStarsPostDao{
         jdbcTemplate.update(SQL_DELETE_POST, postId);
     }
 
-
     @Override
     public List<Post> searchPosts(String searchTerm) {
         searchTerm = "%" + searchTerm + "%";
         List<Post> list;
         try {
-             return list = jdbcTemplate.query(SQL_SELECT_POST_BY_SEARCHTERM, new PostMapper(), searchTerm, searchTerm, searchTerm, searchTerm);
-        } catch (EmptyResultDataAccessException ex){
+            return list = jdbcTemplate.query(SQL_SELECT_POST_BY_SEARCHTERM, new PostMapper(), searchTerm, searchTerm, searchTerm, searchTerm);
+        } catch (EmptyResultDataAccessException ex) {
             return null;
-        }            
-          
+        }
+
     }
 
-
-    
-     private static final class PostMapper implements RowMapper<Post> {
+    private static final class PostMapper implements RowMapper<Post> {
 
         @Override
         public Post mapRow(ResultSet rs, int i) throws SQLException {
-           Post post = new Post();
-           post.setPostId(rs.getInt("PostID"));
+            Post post = new Post();
+            post.setPostId(rs.getInt("PostID"));
             post.setTitle(rs.getString("Title"));
             post.setYear(rs.getInt("PostYear"));
             post.setMonth(rs.getString("PostMonth"));
@@ -179,35 +169,34 @@ public class SoupaStarsPostDaoDBImpl implements SoupaStarsPostDao{
             post.setAuthor(rs.getString("Author"));
             post.setBody(rs.getString("PostBody"));
             post.setCategory(rs.getString("Category"));
-            
+
             return post;
-            
+
         }
-        
+
     }
-     
-     private static final class TagMapper implements RowMapper<String> {
-         
-         @Override
-         public String mapRow(ResultSet rs, int i) throws SQLException {
-             String tagText = rs.getString("TagBody");
-             return tagText;
-         }
-     }
-     
-     private static final class CommentMapper implements RowMapper<Comment> {
-         
-         @Override
-         public Comment mapRow(ResultSet rs, int i) throws SQLException {
-             Comment comment = new Comment();
-             comment.setCommentId(rs.getInt("CommentID"));
-             comment.setDate(rs.getString("commentDate"));
-             comment.setEmail(rs.getString("email"));
-             comment.setName(rs.getString("userName"));
-             comment.setText(rs.getString("commentText"));
-             return comment;
-         }
-     }
+
+    private static final class TagMapper implements RowMapper<String> {
+
+        @Override
+        public String mapRow(ResultSet rs, int i) throws SQLException {
+            String tagText = rs.getString("TagBody");
+            return tagText;
+        }
+    }
+
+    private static final class CommentMapper implements RowMapper<Comment> {
+
+        @Override
+        public Comment mapRow(ResultSet rs, int i) throws SQLException {
+            Comment comment = new Comment();
+            comment.setCommentId(rs.getInt("CommentID"));
+            comment.setDate(rs.getString("commentDate"));
+            comment.setEmail(rs.getString("email"));
+            comment.setName(rs.getString("userName"));
+            comment.setText(rs.getString("commentText"));
+            return comment;
+        }
+    }
 
 }
-
