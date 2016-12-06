4a2ec86928e8d53d7217457f9a54bb7a14b674e5
William Henry Long <william.h.long.1@gmail.com>
2016-12-06 15:20:18 -0500

    finished add, read, and delete static pages

--- SoupaStars/src/main/webapp/js/userPage.js
+++ SoupaStars/src/main/webapp/js/userPage.js
@@ -5,92 +5,31 @@
  */
 
 
-$(function (){
-    loadPosts();
-    
+$(function () {
+    loadStaticPageList();
+
 });
 
-$('#add-button').click(function (event){
-    event.preventDefault();
-    
+function loadStaticPageList() {
     $.ajax({
-        type: 'POST',
-        url: 'post',
-       data: JSON.stringify({
-           title: $('#add-title').val(),
-           date: $('#add-date').val(),
-           category: $('#add-category').val()
-       }),
-       contentType: 'application/json; charset=utf-8',
-       headers: {
-           'Accept': 'applicaiton/json',
-           'Content- Type': 'application/json'
-       },
-       dataType: 'json'
-    }).success(function (){
-        $('#add-title').val('');
-        $('#add-date').val('');
-        $('#add-category').val('');
-        
-        $('#validaitonErrors').empty();
-        loadPosts();
-       }).error(function (data, status){
-           $('#validationErrors').empty();
-           $.each(data.responseJSON.fieldErrors, function (index, validationError){
-               var errorDiv = $('#validationErrors');
-               errorDiv.append(validationError.message.append($('<br>'));
-           });
-       });
-      
-});
-
-$('#edit-button').click(function (event)){
-    event.preventDefault();
-    
-    $.ajax({
-        typ: 'PUT',
-        url: 'post'+ $('#edit-post-id').val(),
-        data: JSON.stringify({
-            postd: $('#edit-post-id').val(),
-            title: $('#edit-title').val(),
-            date: $('#edit- date').val(),
-            category: $('#edit- category').val()
-            
-        }),
-        headers:{
-            'Accept': 'appliction/json',
-            'Content- Type': 'application/json'
-        },
-        'dataType': 'json'
-    }).success(function () {
-        loadPosts();
-    });
-
-function loadPosts(){
-    $.ajax({
-        url: 'post',
+        url: 'staticPages',
         contentType: 'appliction/json',
         dataType: 'json'
-    }).success(function (data, status){
-        fillPostTable(data, status);
-        fillTitleTable(data, status);
-        fillDateTable(data, status);
-        fillCategoryTable(data,staus);
+    }).success(function (data, status) {
+        fillStaticPageTable(data, status);
     });
 }
 
-function fillPostTable(postList, status){
-    clearPostTable();
-    var postTable = $('#postRows');
-    var sortedPosts = postList.sort(funtion (a, b){
-        return b.postId - a.post.Id;
+function fillStaticPageTable(pageList, status) {
+    var pageTable = $('#static-page-table');
+    pageTable.empty();
+
+    $.each(pageList, function (arrayPosition, page) {
+        pageTable.append($('<tr><td><h4><a href="displayStaticPage' + page.pageId +
+                '">' + page.title + '</a></h4></td>'
+                + '<td>' + page.expirationDate + '</td>'
+                + '<td><a class="btn btn-primary" href="#" >Edit Page</a></td>'
+                + '<td><a class="btn btn-primary "href="deleteStaticPage' + page.pageId +'">Delete Page</a></td>'
+                +'</tr>'));
     });
-    $.each(sortedPosts, function(arrayPosition, post){
-        postTable.append($('<tr>')
-                .append($('<h2>' + post.title + '</h2>\n\
-                <p><span class="glyphicon glyphicon-date"></span>Posted on ' + post.month + '' + post.day + ',' + post.year + '&nbsp;\n\
-                <span class="glyphicon glyphicon-duplicate"></span><a href="#"> ' + post.category + ' </a>&nbsp;\n\
-               ) ));
-                
-                
-    });
+}
