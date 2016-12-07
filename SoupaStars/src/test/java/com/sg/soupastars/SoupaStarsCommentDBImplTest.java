a16b94418754459d1f357a2ba9be6478ff78f98f
William Henry Long <william.h.long.1@gmail.com>
2016-12-07 17:54:05 -0500

    fixed comment test

--- SoupaStars/src/test/java/com/sg/soupastars/SoupaStarsCommentDBImplTest.java
+++ SoupaStars/src/test/java/com/sg/soupastars/SoupaStarsCommentDBImplTest.java
@@ -5,10 +5,13 @@
  */
 package com.sg.soupastars;
 
-
 import com.sg.soupastars.dao.SoupaStarsCommentDao;
+import com.sg.soupastars.dao.SoupaStarsPostDao;
 import com.sg.soupastars.model.Comment;
+import com.sg.soupastars.model.Post;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -25,45 +28,58 @@ import static org.junit.Assert.assertNull;
  * @author apprentice
  */
 public class SoupaStarsCommentDBImplTest {
- 
- private SoupaStarsCommentDao dao;
-    
-    public SoupaStarsCommentDBImplTest(){
-        
-        
-   }
+
+    private SoupaStarsCommentDao dao;
+    private SoupaStarsPostDao pdao;
+
+    public SoupaStarsCommentDBImplTest() {
+
+    }
 
     @BeforeClass
-    public static void setUpClass(){
-        
+    public static void setUpClass() {
+
     }
-    
+
     @AfterClass
-    public static void tearDownClass(){
-        
+    public static void tearDownClass() {
+
     }
-    
+
     @Before
-    public void setup(){
-        
+    public void setup() {
+
         ApplicationContext ctx = new ClassPathXmlApplicationContext("test-applicationContext.xml");
         dao = (SoupaStarsCommentDao) ctx.getBean("SoupaStarsCommentDBImpl");
-    
+        pdao = (SoupaStarsPostDao) ctx.getBean("SoupaStarsPostDaoDBImpl");
+
         JdbcTemplate cleaner = (JdbcTemplate) ctx.getBean("jdbcTemplate");
-       cleaner.execute("delete from PostComment");
+        cleaner.execute("delete from PostComment");
         cleaner.execute("delete from Comments");
-
-
-     }
-    
-    @After
-    public void tearDown(){
-        
+        cleaner.execute("delete from Post");
     }
-    
+
+    @After
+    public void tearDown() {
+
+    }
+
     @Test
-    public void addGetDeleteComment(){
-       Comment nc = new Comment();
+    public void addGetDeleteComment() {
+        Post pt = new Post();
+        pt.setTitle("Cookies");
+        pt.setYear(2016);
+        pt.setMonth("December");
+        pt.setDay(02);
+        pt.setAuthor("admin");
+        pt.setBody("hello");
+        pt.setCategory("Food");
+        List<String> tagList = new ArrayList();
+        tagList.add("tag");
+        pt.setTagList(tagList);
+        pdao.addPost(pt);
+        
+        Comment nc = new Comment();
         nc.setName("Alyssa");
         nc.setEmail("arice713@yahoo.com");
         nc.setText("hi");
@@ -76,6 +92,6 @@ public class SoupaStarsCommentDBImplTest {
         assertEquals(fromDb.getText(), nc.getText());
         assertEquals(fromDb.getDate(), nc.getDate());
         dao.removeComment(nc.getCommentId());
-       assertNull(dao.getCommentById(nc.getCommentId()));
-   }
+        assertNull(dao.getCommentById(nc.getCommentId()));
     }
+}
